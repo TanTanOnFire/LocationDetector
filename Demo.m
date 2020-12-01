968996a273ff12130779fbf2cf70a3ff9f819de2
Tannaz Kalatian <46591394+TanTanOnFire@users.noreply.github.com>
2020-12-01 16:34:21 -0500

    Update Demo.m
    Add your train and test files here

--- Demo.m
+++ Demo.m
@@ -1,7 +1,7 @@
 clear;clc;close all
 
-    addTrain='E:\semester 8\vision\project\code_Kalatian\ImageCoordinateDB\Train';
-    addTest='E:\semester 8\vision\project\code_Kalatian\ImageCoordinateDB\Test';
+    addTrain='ImageCoordinateDB\Train';
+    addTest='ImageCoordinateDB\Test';
     [FeatureTrain,addFeaTrain]=ExtractFeature(addTrain);
    [FeatureTest,addFeaTest]=ExtractFeature(addTest);
     %load('tfeat.mat');
